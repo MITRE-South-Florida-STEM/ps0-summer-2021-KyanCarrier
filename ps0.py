ff919131181c0f398c440a93e4f84fb9b4c55070
KyanCarrier <85882960+KyanCarrier@users.noreply.github.com>
2021-07-09 02:29:56 +0000

    Here's my code for assignment 1

--- ps0.py
+++ ps0.py
@@ -1,10 +1,21 @@
+import math
 # 1. Ask the user to enter a number "x"
-
+print("type a number for x")
+x = int(input())
 
 # 2. Ask the user to enter a number "y"
+print("type a second number for y")
+y = int(input())
 
+print("Enter number x: ", end="")
+print(x)
+print("Enter number y: ", end="")
+print(y)
 
 # 3. Prints out the number "x" raised to the power "y"
-
+print("x**y is: ", end="")
+print(x**y)
 
 # 4. Prints out the log (base 2) of "x"
+print("log(x) is: ", end="")
+print(math.log2(x))
